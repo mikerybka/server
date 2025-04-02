ec2f43ede7d84a052c2049a9cf4a035ad80fe31a
Mike Rybka <merybka@gmail.com>
2025-04-02 10:10:57 -0400

    Add frontend proxy logic

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 	"strings"
+
+	"github.com/mikerybka/constants"
+	"github.com/mikerybka/secretdb"
+	"github.com/mikerybka/util"
 )
 
 func main() {
@@ -19,23 +25,76 @@ func main() {
 		return
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "ok")
-	})
-	http.ListenAndServe(":2222", nil)
+	recordError(updateSystem())
+
+	// Run system
+	backendURL, err := url.Parse(fmt.Sprintf("http://%s:3000", constants.BackendIP))
+	if err != nil {
+		panic(err)
+	}
+	handler := httputil.NewSingleHostReverseProxy(backendURL)
+	certDir := filepath.Join(workdir(), "certs")
+	secrets := secretdb.NewClient(constants.BackendIP, "mike", util.RequireEnvVar("SECRET_PASSWORD"))
+	email, _ := secrets.Email()
+	allowHost := func(host string) bool {
+		return len(strings.Split(host, ".")) <= 4
+	}
+	err = util.ServeHTTPS(handler, email, certDir, allowHost)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
-var systemdServiceFile = []byte(`[Unit]
+func updateSystem() error {
+	// Run: apt update
+	cmd := exec.Command("apt", "update")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("apt update: %s", out)
+	}
+
+	// Run: apt upgrade -y
+	cmd = exec.Command("apt", "upgrade", "-y")
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("apt upgrade: %s", out)
+	}
+
+	// Update Go
+	//TODO
+
+	// Update binaries
+	//TODO
+
+	return nil
+}
+
+func workdir() string {
+	return filepath.Join(util.HomeDir(), ".local/share/server")
+}
+
+func recordError(err error) {
+	if err != nil {
+		path := filepath.Join(util.HomeDir(), "errors", util.UnixNanoTimestamp())
+		err = util.WriteFile(path, []byte(err.Error()))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+var systemdServiceFileTemplate = `[Unit]
 Description=Server
 After=network.target
 
 [Service]
 Type=simple
 ExecStart=/bin/server
+Environment=SECRET_PASSWORD="%s"
 
 [Install]
 WantedBy=multi-user.target
-`)
+`
 
 func setup() error {
 	switch runtime.GOOS {
@@ -51,7 +110,12 @@ func setup() error {
 }
 
 func setupSystemd() error {
-	err := os.WriteFile("/etc/systemd/system/server.service", systemdServiceFile, os.ModePerm)
+	password, err := util.GetPasswordFromStdin("Secret password:")
+	if err != nil {
+		return err
+	}
+	b := []byte(fmt.Sprintf(systemdServiceFileTemplate, password))
+	err = os.WriteFile("/etc/systemd/system/server.service", b, os.ModePerm)
 	if err != nil {
 		return fmt.Errorf("writing systemd unit file: %s", err)
 	}
